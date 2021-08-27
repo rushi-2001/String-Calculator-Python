4e8a5c59cb4c98379bbf863095975c315bb3eeaf
rushi-2001 <rushigpatel.01@gmail.com>
2021-08-27 12:17:54 +0530

    single number string return single numbers

--- StringCalculator.py
+++ StringCalculator.py
@@ -9,6 +9,12 @@ def test():
     # test case for empty string
     assert (addStrings("")) == 0, "Empty String didn't return 0"
     
+    # test case for single number
+    assert (addStrings("1")) == 1, "Single number String \"1\" didn't return 1"
+    assert (addStrings("2")) == 2, "Single number String \"2\" didn't return 2"
+    assert (addStrings("42")) == 42, "Single number String \"42\" didn't return 42"
+    
+    
     print("\n===> All Test Cases Passed.\n")
 
 
@@ -17,6 +23,7 @@ def addStrings(numString):
     
     if numString == "":
         return 0
+    return(int(numString))
     
 
 # call test function
